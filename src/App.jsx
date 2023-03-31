daca12dfd9ecb9876dc9cd0dd00f6006b23bb868
tpoluol <109188645+tpoluol@users.noreply.github.com>
2023-03-31 16:06:39 +0900

    rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import star from './assets/icon-star.svg';
+import thank from './assets/illustration-thank-you.svg';
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col justify-center items-center w-screen h-screen">
       <p className="font-bold text-LightGrey font-Overpass ">asdasd</p>
+      <img src={star} alt="star icon" />
+      <img src={thank} alt="thank you" />
     </div>
   );
 }
